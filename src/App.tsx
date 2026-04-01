a353a14cc67f797b71c70afeefd8feb955a4cc0e
doudaoud <boudaoudreda1@gmail.com>
2026-04-01 23:09:41 +0100

    Add routing and new pages for Quiz application
    - Implemented routing using react-router-dom
    - Created Home, Quiz, and Result components
    - Updated App component to include routes
    - Added navigation functionality in Home component

--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,19 @@
 import React from "react";
-import TextType from "./TypeText/TypeText";
 import "./App.css";
-export default function App():React.ReactElement {
+import { Routes, Route } from "react-router-dom";
+import Quiz from "./pages/Quiz";
+import Result from "./pages/Result";
+import Home from "./pages/Home";
+export default function App(): React.ReactElement {
   return (
     <React.Fragment>
-      <div className="container">
-        <h1 className="title" >Quiz Master</h1>
-        <p className="description">
-          Testez vos connaissances générales avec notre quiz interactif. Vous
-          aurez 60 secondes pour répondre à 10 questions.
-        </p>
-        <div  className="information-quiz">
-          <div className="titles">
-            <p>Nombre de questions</p>
-            <p>Temps limite</p>
-            <p>Format</p>
-          </div>
-          <div className="infos">
-            <p className="hello">10</p>
-            <p>60 secondes</p>
-            <p>Choix multiple</p>
-          </div>
-        </div>
-        <button className="commancer-quiz">
-          {" "}
-          <p>
-            {/* Commencer le quiz */}
-            <TextType
-              text={[
-                "Commencer le quiz",
-                "Prêt à relever le défi ?",
-                "Testez vos connaissances maintenant !",
-              ]}
-              typingSpeed={100}
-              pauseDuration={1500}
-              showCursor
-              cursorCharacter="_"
-              deletingSpeed={50}
-              variableSpeedEnabled={false}
-              variableSpeedMin={60}
-              variableSpeedMax={120}
-              cursorBlinkDuration={0.5}
-            />
-          </p>
-        </button>
-      </div>
+      {/* routes */}
+
+      <Routes>
+        <Route path="/quiz" element={<Quiz />} />
+        <Route path="/result" element={<Result />} />
+        <Route path="/" element={<Home />} />
+      </Routes>
     </React.Fragment>
   );
 }
---
+++ src/pages/Home.tsx
@@ -0,0 +1,54 @@
+import { useCallback } from "react";
+import { useNavigate, type NavigateFunction } from "react-router-dom";
+import TextType from "../TypeText/TypeText";
+import "../App.css";
+
+export default function Home() {
+  const navigate: NavigateFunction = useNavigate();
+
+  const handleButtonClick = useCallback(() => {
+    navigate("/quiz");
+  }, [navigate]);
+
+  return (
+    <div className="container">
+      <h1 className="title">Quiz Master</h1>
+      <p className="description">
+        Testez vos connaissances générales avec notre quiz interactif. Vous
+        aurez 60 secondes pour répondre à 10 questions.
+      </p>
+      <div className="information-quiz">
+        <div className="titles">
+          <p>Nombre de questions</p>
+          <p>Temps limite</p>
+          <p>Format</p>
+        </div>
+        <div className="infos">
+          <p className="hello">10</p>
+          <p>60 secondes</p>
+          <p>Choix multiple</p>
+        </div>
+      </div>
+      <button className="commancer-quiz" onClick={handleButtonClick} title="Commencer le quiz">
+        <p>
+          <TextType
+            text={[
+              "Commencer le quiz",
+              "Prêt à relever le défi ?",
+              "Testez vos connaissances maintenant !",
+            ]}
+            typingSpeed={100}
+            pauseDuration={1500}
+            showCursor
+            cursorCharacter="_"
+            deletingSpeed={50}
+            variableSpeedEnabled={false}
+            variableSpeedMin={60}
+            variableSpeedMax={120}
+            cursorBlinkDuration={0.5}
+          />
+        </p>
+      </button>
+    </div>
+  );
+}
