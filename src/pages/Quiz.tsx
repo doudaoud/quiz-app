bf4ca0cf8e04e8e0e4089613c80b70f574b81828
doudaoud <boudaoudreda1@gmail.com>
2026-04-03 00:56:59 +0100

    Add quiz container and progress bar components with styling

--- src/pages/Quiz.tsx
+++ src/pages/Quiz.tsx
@@ -1,7 +1,24 @@
-import React from 'react'
+import React from "react";
 
 export default function Quiz() {
+  const [value, setValue] = React.useState<number>(100)
   return (
-    <div>Quiz</div>
-  )
+    <React.Fragment>
+      <div className="container_quiz">
+        <div className="progress">
+          <div className="progress-fill" style={{ width: `${value}%` }} />
+        </div>
+        <p>Temps restant</p>
+        <input
+          type="range"
+          className="qq"
+          style={{
+            pointerEvents: "none",
+            color: "rgb(79, 70, 229)",
+          }}
+        />
+        <div className="cadre_quiz"></div>
+      </div>
+    </React.Fragment>
+  );
 }
