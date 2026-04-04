106e6063d8b8f6f89c2437407c5968ce6b5bc528
doudaoud <boudaoudreda1@gmail.com>
2026-04-04 22:22:03 +0100

    Enhance Quiz component layout with new response options and styling for improved user interaction

--- src/pages/Quiz.tsx
+++ src/pages/Quiz.tsx
@@ -17,14 +17,52 @@ export default function Quiz() {
           <div className="progress-fill" style={{ width: `${value}%` }} />
         </div>
         <div className="cadre_quiz">
-          <h2 className="qustion">
-            Quel est le plus grand océan du monde ?
-          </h2>
-          <p className="num_question">
-            Question 1 sur 10
-          </p>
+          <h2 className="qustion">Quel est le plus grand océan du monde ?</h2>
+          <p className="num_question">Question 1 sur 10</p>
           <div className="reponses">
-            
+            <label className="carte">
+              <input
+                type="radio"
+                name="res"
+                id="1"
+                title="question 1"
+                className="radio"
+              />
+              <span>question 1 </span>
+            </label>
+            <label className="carte">
+              <input
+                type="radio"
+                name="res"
+                id="2"
+                title="question 1"
+                className="radio"
+              />
+              <span>question 1 </span>
+            </label>{" "}
+            <label className="carte">
+              <input
+                type="radio"
+                name="res"
+                id="3"
+                title="question 1"
+                className="radio"
+              />
+              <span>question 1 </span>
+            </label>{" "}
+            <label className="carte">
+              <input
+                type="radio"
+                name="res"
+                id="4"
+                title="question 1"
+                className="radio"
+              />
+              <span>question 1 </span>
+            </label>
+          
+            <button className="suivant" disabled >Suivant</button>
+         
           </div>
         </div>
       </div>
