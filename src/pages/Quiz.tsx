a22d33a2ded2574404c894803ee7687fc0e6040c
doudaoud <boudaoudreda1@gmail.com>
2026-04-04 17:41:05 +0100

    Refactor Quiz component layout and styles for improved readability and structure

--- src/pages/Quiz.tsx
+++ src/pages/Quiz.tsx
@@ -1,23 +1,32 @@
 import React from "react";
 
 export default function Quiz() {
-  const [value, setValue] = React.useState<number>(100)
+  const [time, setTime] = React.useState<string | null>();
+  const [value, setValue] = React.useState<number>(100);
   return (
     <React.Fragment>
       <div className="container_quiz">
+        <h3>Time left:</h3>
         <div className="progress">
           <div className="progress-fill" style={{ width: `${value}%` }} />
         </div>
-        <p>Temps restant</p>
-        <input
-          type="range"
-          className="qq"
-          style={{
-            pointerEvents: "none",
-            color: "rgb(79, 70, 229)",
-          }}
-        />
-        <div className="cadre_quiz"></div>
+        <p className="temps_restant">Temps restant</p>
+
+        <p className="question_num">Question sur 10</p>
+        <div className="progress">
+          <div className="progress-fill" style={{ width: `${value}%` }} />
+        </div>
+        <div className="cadre_quiz">
+          <h2 className="qustion">
+            Quel est le plus grand océan du monde ?
+          </h2>
+          <p>
+            Question 1 sur 10
+          </p>
+          <div className="reponses">
+            
+          </div>
+        </div>
       </div>
     </React.Fragment>
   );
